53d9320158b4618d8c4af5a9d906bafc7a5c498b
whosl <982267311@qq.com>
2019-12-10 22:59:40 +0800

    暂时去除Eureka

--- src/main/java/buaa/group6/scienceshare/ScienceShareApplication.java
+++ src/main/java/buaa/group6/scienceshare/ScienceShareApplication.java
@@ -2,12 +2,10 @@ package buaa.group6.scienceshare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
-@EnableEurekaClient
 public class ScienceShareApplication {
 
     public static void main(String[] args) {
